f05ac7486309f50a7597bde28a37f87a90529d76
Max <max.blum@gmail.com>
2017-01-23 15:17:53 -0500

    Add mentor optimization code

--- mentors.ml
+++ mentors.ml
@@ -104,3 +104,97 @@ module Optimize (M : Optimizable) = struct
   let optimize = _optimize true
   let optimize_with_stats = _optimize false
 end
+
+module MentorPrefs = struct
+  type pref = int * int
+
+  type input = (string * (int list)) list
+  type output = (string * int) list
+  type state = (string * int * (int list)) list
+
+  let scored_pref : int list -> pref list = function
+    | [first; second; third; fourth] ->
+        [(first, 14); (second, 13); (third, 12); (fourth, 11)]
+    | [first; second; third] ->
+        [(first, 14); (second, 13); (third, 12)]
+    | _ -> []
+
+  let convert_pref (n,pref) = match pref with
+    | [] -> failwith "Each student must have at least one preference"
+    | p :: ps -> (n, p, pref)
+
+  let state_of_data = List.map convert_pref
+  let output_of_state = List.map (fun (name,mentor,prefs) -> (name,mentor))
+
+  let rec get_one_score pref m =
+    match pref with
+    | (pm, s) :: ps ->
+        if pm=m then s
+        else get_one_score ps m
+    | [] -> failwith "Should only assign chosen mentors"
+
+  let rec score_set_rec assignments taken score count = match assignments with
+  | [] -> let diff = (TreeSet.size taken) - count in
+    if diff != 0 then diff else score
+  | (n,m,pref) :: rest ->
+    score_set_rec rest (TreeSet.add m taken) (score + get_one_score (scored_pref pref) m) (count+1)
+
+  let score_set assignments = score_set_rec assignments TreeSet.empty 0 0
+  let eval a1 a2 = (score_set a2) - (score_set a1)
+
+  let rec change_member changer i = function
+    | [] -> failwith "change_member index should be less than list length"
+    | x :: xs ->
+        if i=0 then (changer x) :: xs
+        else x :: change_member changer (i-1) xs
+
+  let change_random_member changer l = change_member changer (List.length l |> Random.int) l
+
+  let rec pick_random_rec cur seen = function
+    | [] -> cur
+    | x :: xs ->
+        let choice = (if Random.int (seen+1) = 0 then Some x else cur)
+        in pick_random_rec choice (seen+1) xs
+
+  let pick_random = pick_random_rec None 0
+
+  let switch_mentor (n, m, pref) =
+    let new_choice =
+      match pick_random (List.filter ((!=) m) pref) with
+      | None -> failwith "Should have found a new mentor choice"
+      | Some c -> c
+      in (n, new_choice, pref)
+
+  let iter = change_random_member switch_mentor
+
+  let rec get_rank_rec (n,m,pref) count = match pref with
+    | [] -> failwith "Must have at least one preference"
+    | p :: ps -> if m=p then count else get_rank_rec (n,m,ps) (count+1)
+
+  let get_rank a = get_rank_rec a 1
+
+  let mean l =
+    let sum = float_of_int (list_sum l) in
+    let length = float_of_int (List.length l) in
+    sum /. length
+
+  let mean_rank a = a |> List.map get_rank |> mean
+
+  let stats_of_state a =
+    let score = score_set a in
+    if score < 0 then "Failed.\n"
+    else (
+      "Score: " ^ (string_of_int score) ^ "\n" ^
+      "1sts: " ^ (a |> List.map get_rank |> List.find_all ((=) 1) |> List.length |> string_of_int) ^ "\n" ^
+      "2nds: " ^ (a |> List.map get_rank |> List.find_all ((=) 2) |> List.length |> string_of_int) ^ "\n" ^
+      "3rds: " ^ (a |> List.map get_rank |> List.find_all ((=) 3) |> List.length |> string_of_int) ^ "\n" ^
+      "4rds: " ^ (a |> List.map get_rank |> List.find_all ((=) 4) |> List.length |> string_of_int) ^ "\n" ^
+      "Mean rank: " ^ (a |> mean_rank |> string_of_float)
+    )
+end
+
+module PrefsOptimizer = Optimize(MentorPrefs)
+
+let generate_requests student_count mentor_count =
+  let gen_student = fun () -> ("Jim", List.map (fun () -> 1 + Random.int mentor_count) [(); (); (); ()]) in
+  gen_list gen_student student_count
