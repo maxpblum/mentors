a02e73fb77b5219fffa9d5bc4f2062e7e25894c9
Max <max.blum@gmail.com>
2017-01-24 12:14:39 -0500

    Use correct type for sample_prefs

--- mentors.ml
+++ mentors.ml
@@ -2,27 +2,27 @@ Random.init 10;;
 
 type request = {name:string; prefs:int list}
 
-let sample_prefs : (string * (int list)) list = [
-  ("Jim", [17; 26; 26; 25]);
-  ("Jim", [22; 13; 26; 33]);
-  ("Jim", [12; 4; 2; 23]);
-  ("Jim", [38; 18; 12; 10]);
-  ("Jim", [3; 15; 36; 1]);
-  ("Jim", [33; 1; 39; 29]);
-  ("Jim", [5; 35; 32; 19]);
-  ("Jim", [38; 7; 16; 21]);
-  ("Jim", [18; 39; 20; 26]);
-  ("Jim", [39; 17; 32; 13]);
-  ("Jim", [25; 8; 18; 33]);
-  ("Jim", [15; 25; 3; 15]);
-  ("Jim", [28; 25; 8; 20]);
-  ("Jim", [15; 21; 1; 13]);
-  ("Jim", [38; 32; 31; 4]);
-  ("Jim", [31; 19; 31; 2]);
-  ("Jim", [18; 38; 34; 35]);
-  ("Jim", [5; 29; 2; 31]);
-  ("Jim", [24; 22; 30; 36]);
-  ("Jim", [6; 14; 17; 14]);
+let sample_prefs : request list = [
+  {name="Jim"; prefs=[17; 26; 26; 25]};
+  {name="Jim"; prefs=[22; 13; 26; 33]};
+  {name="Jim"; prefs=[12; 4; 2; 23]};
+  {name="Jim"; prefs=[38; 18; 12; 10]};
+  {name="Jim"; prefs=[3; 15; 36; 1]};
+  {name="Jim"; prefs=[33; 1; 39; 29]};
+  {name="Jim"; prefs=[5; 35; 32; 19]};
+  {name="Jim"; prefs=[38; 7; 16; 21]};
+  {name="Jim"; prefs=[18; 39; 20; 26]};
+  {name="Jim"; prefs=[39; 17; 32; 13]};
+  {name="Jim"; prefs=[25; 8; 18; 33]};
+  {name="Jim"; prefs=[15; 25; 3; 15]};
+  {name="Jim"; prefs=[28; 25; 8; 20]};
+  {name="Jim"; prefs=[15; 21; 1; 13]};
+  {name="Jim"; prefs=[38; 32; 31; 4]};
+  {name="Jim"; prefs=[31; 19; 31; 2]};
+  {name="Jim"; prefs=[18; 38; 34; 35]};
+  {name="Jim"; prefs=[5; 29; 2; 31]};
+  {name="Jim"; prefs=[24; 22; 30; 36]};
+  {name="Jim"; prefs=[6; 14; 17; 14]};
 ]
 
 let list_sum = List.fold_left (+) 0
