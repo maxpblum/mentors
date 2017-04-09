6db545c7f4e8e39728b6bfd0306d7ea97bf0a87d
Max <max.blum@gmail.com>
2017-04-09 08:52:19 -0400

    Test has

--- test/treeCounter.js
+++ test/treeCounter.js
@@ -2,7 +2,13 @@ const TreeCounter = require('../src/treeCounter.js');
 const {expect} = require('chai');
 
 describe('TreeCounter', () => {
-  it('initializes successfully', () => {
-    expect(TreeCounter.empty());
+  it('initializes successfully', () => expect(TreeCounter.empty()));
+
+  it('keeps track of members', () => {
+    const nums = [4, 6, 1, 13];
+    const withFourMembers = nums.reduce(
+      (counter, num) => counter.inc(num),
+      TreeCounter.empty());
+    nums.forEach(num => expect(withFourMembers.has(num)));
   });
 });
